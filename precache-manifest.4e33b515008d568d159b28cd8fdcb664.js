9f030ce7dce1d19e9c150cbb637d7d610f97f702
aneesh <aneeshpanoli@gmail.com>
2020-06-08 14:50:38 -0700

    first commit

---
+++ precache-manifest.4e33b515008d568d159b28cd8fdcb664.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1747ff0e5138f2b3556092d1acd3a852",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0aff4d960c6ed3be2232",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2687a0d42e92b1d31688",
+    "url": "/static/js/2.4fec9471.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/static/js/2.4fec9471.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aff4d960c6ed3be2232",
+    "url": "/static/js/main.4f477634.chunk.js"
+  },
+  {
+    "revision": "a56fc0749b3eaa0e4ba2",
+    "url": "/static/js/runtime-main.e07bf59f.js"
+  }
+]);
